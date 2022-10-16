c53cfaa2364adb757e9f3801414a8ac3908c882c
Eddy <kimkyunghun3@naver.com>
2022-10-16 12:46:02 +0900

    feat: CoreData로 Local DB 구현 및 폴더화

---
+++ Book-Store-Eddy/Storage/BookEntity+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  BookEntity+CoreDataClass.swift
+//  Book-Store-Eddy
+//
+//  Created by Eddy on 2022/10/15.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(BookEntity)
+public class BookEntity: NSManagedObject {
+
+}
